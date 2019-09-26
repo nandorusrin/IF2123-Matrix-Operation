fcc9e74ea06c11020306812f802840d2f670e9f2
miner46er <stefanus.ardimulia@gmail.com>
2019-09-26 23:22:34 +0700

    fix interpolasi

--- src/MatriksInterpolasi.java
+++ src/MatriksInterpolasi.java
@@ -10,17 +10,17 @@ public class MatriksInterpolasi extends Matriks {
     // Interpolasi
     public void interpolasi(){
         // proses menghitung interpolasi
-        for (int i = 1; i < this.brs; i++) {
-            for (int j = 1; j < this.kol; j++) {
-                this.data[i][j]= java.lang.Math.pow(this.data[i][j],j-1);
+        for (int i = 1; i <= this.brs; i++) {
+            for (int j = 1; j <= this.kol; j++) {
+                this.data[i][j] = java.lang.Math.pow(this.data[i][j],j-1);
                 this.gaussJordan();
             }
         }
         system.out.printf("y= ");
         system.out.printf("%.1f ", this.data[1][this.kol]);
-        for (int i = 2; i < this.brs; i++){
-            if (0!= this.data[i][this.kol]);{
-                if (this.data[i][this.kol] >=0){
+        for (int i = 2; i <= this.brs; i++){
+            if (0 != this.data[i][this.kol]){
+                if (this.data[i][this.kol] >= 0){
                     system.out.printf("+");
                 }
                 system.out.printf("%.1fx^%d ", this.data[i][this.kol], i-1);
